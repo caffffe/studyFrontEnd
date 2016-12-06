c239ba2d82103c23627c800a037adada6519882e
caffffe <24320132202488@stu.xmu.edu.cn>
2016-12-06 16:41:48 +0800

    task18 js操作队列+排序，只写了冒泡排序，看完网易课堂的算法之后再补上其他排序

--- Baidu IFE_2016/task_2_18_1/readme.md
+++ Baidu IFE_2016/task_2_18_1/readme.md
@@ -4,3 +4,34 @@
 这一点参见上面第二句标红的话就明白什么意思了。 
 不要把<button>标签当成<form>中的input元素。 
 所以每“提交”一次都刷新了页面，相当于没有做任何改变，哭。。。。纠结了好久
+2、闭包问题
+for(var i=0,len=con.childNodes.length;i<len;i++){
+		if(con.childNodes[i]==event.target){
+			var text=con.childNodes[i].innerText;
+			con.childNodes[i].style.opacity="0";
+			setTimeout(function(){
+               con.removeChild(con.childNodes[i]);
+               alert(i);//5
+               alert(text);
+			},1000);
+			
+			
+}
+因为一直报错con.childNodes[i]为Undefined，非常不能理解，原来是i=5，只有四个元素con.childNodes[i]肯定为undefined啦。
+改正为
+for(var i=0,len=con.childNodes.length;i<len;i++){
+		if(con.childNodes[i]==event.target){
+			var text=con.childNodes[i].innerText;
+			con.childNodes[i].style.opacity="0";
+			setTimeout(function(i){
+				return function(){
+                  con.removeChild(con.childNodes[i]);
+                  //alert(i);/
+                  alert(text);
+                  return i;
+                 }
+		   }(i),500);
+			
+			
+		}
+	}
--- Baidu IFE_2016/task_2_19_1/task19.js
+++ Baidu IFE_2016/task_2_19_1/task19.js
@@ -15,18 +15,40 @@ var EventUtil={
 };
 function leftIn(){ 
 	var text=document.getElementsByTagName("input")[0].value;
-	if(text=="")
+	var num=parseInt(text);
+	var boxs=document.getElementsByClassName("box");
+	var len=boxs.length;
+    if(len>=60){
+    	alert("队列数量不能超过60个");
+    }
+	if(isNaN(num)||num<10||num>100){
+		alert("输入数字要在10-100之间")
 		return;
-	var addhtml="<div class='box' onclick='removeOwn()'>"+text+"</div>";
+	}
+	var con=document.getElementById("container");
+	var height=(num/100)*(parseInt(con.offsetHeight));
+    
+	var addhtml="<div class='box' style='height:"+height+"px;' onclick='removeOwn()'></div>";
 	var html=addhtml+con.innerHTML;
     con.innerHTML=html;
 }
 
 function rightIn(){	
 	var text=document.getElementsByTagName("input")[0].value;
-	if(text=="")
+	var num=parseInt(text);
+    var boxs=document.getElementsByClassName("box");
+     var len=parseInt(boxs.length);
+	if(len>=60){
+    	alert("队列数量不能超过60个");
+    }
+	if(isNaN(num)||num<10||num>100){
+		alert("输入数字要在10-100之间");
 		return;
-	var addhtml="<div class='box' onclick='removeOwn()'>"+text+"</div>";
+	}
+	var con=document.getElementById("container");
+	var height=(num/100)*(parseInt(con.offsetHeight));
+
+	var addhtml="<div class='box' style='height:"+height+"px;' onclick='removeOwn()'></div>";
 	var html=con.innerHTML+addhtml;
     con.innerHTML=html;
 }
@@ -45,7 +67,7 @@ function removeOwn(){
 				return function(){
                   con.removeChild(con.childNodes[i]);
                   //alert(i);/
-                  //alert(text);
+                  alert(text);
                   return i;
                  }
 		   }(i),500);
@@ -53,6 +75,23 @@ function removeOwn(){
 			
 		}
 	}
+}
+function swap(box1,box2){
+	var temp=box1.style.height;
+	box1.style.height=box2.style.height;
+	box2.style.height=temp;
+}
+//冒泡排序
+function bubbleSort(){
+   var boxs=document.getElementsByClassName("box");
+   var len=parseInt(boxs.length);
+   for(var i=0;i<len;i++){
+   	  for(var j=i+1;j<len;j++){
+   	  	 if((parseInt(boxs[i].style.height))>(parseInt(boxs[j].style.height))){
+   	  	 	  swap(boxs[i],boxs[j]);
+   	  	 }
+   	  }
+   }
 }
 window.onload=function(){
   var leftInBtn=document.getElementById("leftIn");
@@ -63,6 +102,8 @@ window.onload=function(){
   EventUtil.addHandler(rightInBtn,"click",rightIn);
  var rightOutBtn=document.getElementById("rightOut");
  EventUtil.addHandler(rightOutBtn,"click",rightOut);
+ var bubbleSortBtn=document.getElementById("bubbleSort");
+ EventUtil.addHandler(bubbleSortBtn,"click",bubbleSort);
 };
 
 
